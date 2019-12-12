729a07d1d304b88ae55b6be499c6c2e331bff4ba
Simon Campbell <simon@launchsoftware.nz>
2019-12-12 18:37:06 +1300

    Add failing project, readme and build output

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace csharp_range_expression_crash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var predicate =
+                CreateCrashingExpression()
+                    .Compile();
+
+            var result = predicate("xxxxxRIGHTSUFFIX");
+
+            // Should be true if the compiler did not
+            // crash!
+            Console.WriteLine(result);
+        }
+
+        public static Expression<Func<string, bool>> CreateCrashingExpression()
+        {
+            // Create an expression that compares the "tail" of the string value
+            // with some known value.
+            return (string a) => a[7..] == "RIGHTSUFFIX";
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+Using range syntax to create an expression crashes the compiler. See build-output.log for crash details.
+
+This can be reproduced using .NET Core SDK 3.1.100 and running `dotnet build` in the root folder of this repository.
